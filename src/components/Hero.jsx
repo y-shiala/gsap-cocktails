4b885b939db5c82a300024b42b1efa7d59f6d7fa
y-shiala <yvanashivon@gmail.com>
2025-09-25 09:39:12 +0300

    Added the cocktails section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,13 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import React from 'react'
+import { useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767})
+
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' });
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -37,6 +42,24 @@ const Hero = () => {
       .to('.right-leaf', { y: 200 }, 0)
       .to('.left-leaf', { y: -200 }, 0)
 
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';  
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true
+      }
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration
+      })
+    }
 
   }, []);
 
@@ -65,6 +88,17 @@ const Hero = () => {
         </div>
 
       </section>
+
+      <div className="video absolute inset-0 ">
+        <video 
+        ref={videoRef}
+        src="/videos/output.mp4"
+        muted
+        playsInline
+        preload='auto'
+        
+        / >
+      </div>
     </>
   )
 }
